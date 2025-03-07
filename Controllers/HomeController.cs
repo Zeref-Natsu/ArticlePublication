36fc5453408c885132c929a3614305164e7dfcfa
立祈 陳 <knock2608@gmail.com>
2025-03-08 01:55:37 +0800

    設定好網站首頁草稿

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace 文章寫作平台.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Database=account;User ID=LAPTOP-G5P63730\\KEN;Trusted_Connection=True";
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -17,10 +17,53 @@ namespace 文章寫作平台.Controllers
         {
             ViewData["BodyClass"] = "sub_page";   // 此用於頁面上出現個空白框
             TempData["Account"] = "未登入";  // 顯示是否有登入，有的話顯示登入的帳號
-            
+            TempData["Account_Ref"] = "/Home/login";   // 設定 "登入" 連結的路徑
+
+            //TempData["isLogin1"] = "生活";
+            //TempData["isLogin2"] = "娛樂";
+            //TempData["isLogin3"] = "企業";
             TempData.Keep();  // 用於讓 TempData 的值保存不刪除
             return View();
         }
+
+        public IActionResult MyArticle()
+        {
+            ViewData["BodyClass"] = "sub_page";   // 此用於頁面上出現個空白框
+            TempData["Account"] = TempData["Account"];  // 顯示是否有登入，有的話顯示登入的帳號
+            TempData["Account_Ref"] = TempData["Account_Ref"];
+
+            //TempData["isLogin1"] = "生活";
+            //TempData["isLogin2"] = "娛樂";
+            //TempData["isLogin3"] = "企業";
+            TempData.Keep();  // 用於讓 TempData 的值保存不刪除
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewData["BodyClass"] = "sub_page";   // 此用於頁面上出現個空白框
+
+            TempData["Login"] = "/Home/index";  // 確認會員身分後設定回到哪個首頁
+            TempData["Account"] = "未登入";  // 顯示是否有登入，有的話顯示登入的帳號
+            TempData["Account_Ref"] = "/Home/login";  // 判斷是否為登入狀態後，設定 "登入" 的路徑
+
+            TempData["isLogin1"] = "";  // 確定是否為登入狀態，設定登入狀態的下拉選單[修改會員資料]
+            TempData["isLogin2"] = "登入";  // 確定是否為登入狀態，不是就設定登入狀態的下拉選單[登入]
+            TempData["isLogin3"] = "";  // 確定是否為登入狀態，是舊設定登入狀態的下拉選單[登出]
+
+            TempData.Keep("Login");  // 用於讓 TempData 的值保存不刪除
+            TempData.Keep("Account");  // 用於讓 TempData 的值保存不刪除
+            TempData.Keep("Account_Ref");  // 用於讓 TempData 的值保存不刪除
+            TempData.Keep("isLogin1");  // 用於讓 TempData 的值保存不刪除
+            TempData.Keep("isLogin2");  // 用於讓 TempData 的值保存不刪除
+            TempData.Keep("isLogin3");  // 用於讓 TempData 的值保存不刪除
+            return View();
+        }
+
+
+
+
+
 
         public IActionResult Privacy()
         {
---
+++ Models/Entity/Articles.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Farmer_Project.Models.Entity
+{
+    public class Articles
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ArticlesId { set; get; }
+
+        [ForeignKey("FarmersInfo")]
+        public int? Id { set; get; }
+    
+        [StringLength(15)]
+        public string ArticleType { set; get; }
+
+        [MaxLength(100)]
+        public string ArticleTitle { get; set; }
+
+        [StringLength(50)]
+        public string ArticleImagePath { set; get; }
+
+        public string ArticleSummary { set; get; }
+
+        public bool IsPublished { set; get; } = false;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        //導覽屬性(一對"多")
+        //public virtual FarmersInfo FarmersInfo { get; set; }
+
+        //導覽屬性("一"對多)
+        //public virtual List<FarmersArticlesDetails> FarmersArticlesDetails { get; set; }
+    }
+}
